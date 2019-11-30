1144aa074420e637b0ddac7a5f4fd0f4c85e3ba8
Rameswaree <43037382+Rameswaree@users.noreply.github.com>
2019-11-30 02:05:54 +0000

    Main file committed

---
+++ src/main/java/com/bpdts/BpdtsApplication.java
@@ -0,0 +1,12 @@
+package com.bpdts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class BpdtsApplication{
+
+    public static void main(String[] args){
+    
+        SpringApplication.run(BpdtsApplication.class, args);
+    }
+}
