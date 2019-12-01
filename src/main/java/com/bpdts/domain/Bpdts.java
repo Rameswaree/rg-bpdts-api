813dc099de39879b98d71bb5145f14beb5696d11
Rameswaree <43037382+Rameswaree@users.noreply.github.com>
2019-12-01 11:33:09 +0000

    Update Bpdts.java

--- src/main/java/com/bpdts/domain/Bpdts.java
+++ src/main/java/com/bpdts/domain/Bpdts.java
@@ -1,4 +1,34 @@
 package com.bpdts.domain;
 
-public class Bpdts {
+@Entity
+@Table(name = "tblBpdts")
+public class Bpdts implements Serializable{
+  
+  private static final long serialVersionUID = 7280549021416983975L;
+  
+    @Column(name = "intBpdtsId", nullable = false)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+  
+    @Column(name = "firstName", nullable = true)
+    private String firstName;
+  
+   @Column(name = "lastName", nullable = true)
+    private String lastName;
+  
+  @Column(name = "email", nullable = true)
+    private String email;
+  
+  @Column(name = "ip_address", nullable = true)
+    private String ip_address;
+  
+  @Column(name = "latitude", nullable = true)
+    private double latitude;
+  
+  @Column(name = "longitude", nullable = true)
+    private String longitude;
+  
+  @Column(name = "city", nullable = true)
+    private String city;
 }
