6296d7c25443588872921954dbe057e11588bbcd
Rameswaree Ganesh <rameswaree@gmail.com>
2019-12-01 08:46:51 +0000

    Added application.properties file

--- src/main/java/com/bpdts/dto/BpdtsDto.java
+++ src/main/java/com/bpdts/dto/BpdtsDto.java
@@ -1,5 +1,98 @@
 package com.bpdts.dto;
 
-public class BpdtsDto{
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import java.io.Serializable;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(propOrder = {"id", "first_name", "last_name", "email", "ip_address", "latitude", "longitude", "city"})
+@JsonPropertyOrder({"id", "first_name", "last_name", "email", "ip_address", "latitude", "longitude", "city"})
+@XmlRootElement(name = "instructions", namespace = "##default")
+public class BpdtsDto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private int id = 1;
+
+    private String first_name = "Reemu";
+
+    private String last_name="Ganesh";
+
+    private String email="rameswaree@gmail.com";
+
+    private String ip_address="10.102.14.129";
+
+    private double latitude=50.126485;
+
+    private  double longitude=-51.0326485;
+
+    private String city;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getIp_address() {
+        return ip_address;
+    }
+
+    public void setIp_address(String ip_address) {
+        this.ip_address = ip_address;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 }
--- src/main/java/com/bpdts/web/BpdtsController.java
+++ src/main/java/com/bpdts/web/BpdtsController.java
@@ -1,8 +1,24 @@
 package com.bpdts.web;
 
+import com.bpdts.dto.BpdtsDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BpdtsController{
 
+        @GetMapping("/instructions")
+        public ResponseEntity<BpdtsDto> getUserList() {
+
+            BpdtsDto bpdtsDto = null;
+            try{
+                bpdtsDto = new BpdtsDto();
+            }catch(Exception e){
+
+                return new ResponseEntity(null, HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity(bpdtsDto, HttpStatus.OK);
+        }
 }
